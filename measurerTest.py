e1635e6b4cfc8afb8735898a5b8d63836af29f11
ali <ali-heidari@outlook.com>
2020-02-23 23:35:53 +0330

    First try to add unittest

---
+++ measurerTest.py
@@ -0,0 +1,12 @@
+import unittest 
+
+
+
+class MeasurerTest(unittest.TestCase): 
+  
+    # Returns True or False.  
+    def test(self):         
+        self.assertTrue(True) 
+  
+if __name__ == '__main__': 
+    unittest.main() 
